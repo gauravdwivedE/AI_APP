b4058018ff19ac9a4f76a8d543f71fd97fca6f2a
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-11-30 14:28:27 +0530

    fix: cors error

--- API/src/app.js
+++ API/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require('./routes/chat.routes');
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
